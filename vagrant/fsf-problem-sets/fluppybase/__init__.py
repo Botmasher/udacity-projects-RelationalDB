b0fe96a9d4fa6cf3af9db8d4890f497f7d814954
Botmasher <languagejosh@hotmail.com>
2015-12-07 15:00:09 -0500

    Remove flask-test package and add sqlalchemy extension for SQLAlchemy class

--- vagrant/fsf-problem-sets/fluppybase/__init__.py
+++ vagrant/fsf-problem-sets/fluppybase/__init__.py
@@ -1,6 +1,7 @@
 # start Flask app in subfolder following Simple Package structure
 # http://flask.pocoo.org/docs/0.10/patterns/packages/#simple-packages
 from flask import Flask
+
 app = Flask (__name__)
 app.config.from_object('config')
 
